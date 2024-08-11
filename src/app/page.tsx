d3b3abcf3366b221364bba375c3c2f9b16f13b20
Waqas Nazir <waqasnazir@altaurux.com>
2024-08-11 14:20:43 +0500

    UI Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,53 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="mx-[5%] my-6  h-fit p-4 rounded bg-green-300 text-black flex flex-col gap-2">
+      <h1 className=" text-2xl">Description</h1>
+
+      <p className=" text-justify">
+        NestJS, Next.js, React, and Node.js are powerful tools for modern web
+        development, each serving distinct roles. Node.js is a server-side
+        runtime that enables JavaScript execution on the backend, offering a
+        non-blocking, event-driven architecture ideal for scalable applications.
+        NestJS builds on Node.js, providing a robust framework with a modular
+        architecture and TypeScript-first approach, making it perfect for
+        developing scalable, maintainable server-side applications. On the
+        frontend, React.js is a JavaScript library focused on building dynamic
+        user interfaces through its component-based architecture and virtual DOM
+        for efficient rendering. Next.js extends React by adding server-side
+        rendering, static site generation, and a rich set of features for
+        building optimized, production-ready applications. Together, these
+        frameworks enable developers to create full-stack applications with a
+        seamless developer experience and efficient performance. NestJS,
+        Next.js, React, and Node.js are powerful tools for modern web
+        development, each serving distinct roles. Node.js is a server-side
+        runtime that enables JavaScript execution on the backend, offering a
+        non-blocking, event-driven architecture ideal for scalable applications.
+        NestJS builds on Node.js, providing a robust framework with a modular
+        architecture and TypeScript-first approach, making it perfect for
+        developing scalable, maintainable server-side applications. On the
+        frontend, React.js is a JavaScript library focused on building dynamic
+        user interfaces through its component-based architecture and virtual DOM
+        for efficient rendering. Next.js extends React by adding server-side
+        rendering, static site generation, and a rich set of features for
+        building optimized, production-ready applications. Together, these
+        frameworks enable developers to create full-stack applications with a
+        seamless developer experience and efficient performance. NestJS,
+        Next.js, React, and Node.js are powerful tools for modern web
+        development, each serving distinct roles. Node.js is a server-side
+        runtime that enables JavaScript execution on the backend, offering a
+        non-blocking, event-driven architecture ideal for scalable applications.
+        NestJS builds on Node.js, providing a robust framework with a modular
+        architecture and TypeScript-first approach, making it perfect for
+        developing scalable, maintainable server-side applications. On the
+        frontend, React.js is a JavaScript library focused on building dynamic
+        user interfaces through its component-based architecture and virtual DOM
+        for efficient rendering. Next.js extends React by adding server-side
+        rendering, static site generation, and a rich set of features for
+        building optimized, production-ready applications. Together, these
+        frameworks enable developers to create full-stack applications with a
+        seamless developer experience and efficient performance.
+      </p>
     </div>
   );
 }
--- src/components/BlogsSidebar/index.tsx
+++ src/components/BlogsSidebar/index.tsx
@@ -1,18 +1,52 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const BlogsSidebar = () => {
+  const currentPath = usePathname();
+
+  console.log("path:", currentPath);
+
   return (
     <div className="w-[20%] flex flex-col gap-8 px-10 py-10 bg-black">
-      <ul className="flex flex-col gap-5 text-lg font-normal ">
-        <li>
-          <Link href="/blogs">Blogs</Link>
+      <ul className="flex flex-col gap-5 text-lg font-normal">
+        <li className="w-full">
+          <Link
+            className={`w-full ${
+              currentPath === "/blogs"
+                ? "w-full bg-green-400 rounded-sm px-2 py-1"
+                : ""
+            }`}
+            href="/blogs"
+          >
+            Blogs
+          </Link>
         </li>
         <li>
-          <Link href="/blogs/frontend-blogs">Frontend Blogs</Link>
+          <Link
+            className={`w-full ${
+              currentPath === "/blogs/frontend-blogs"
+                ? "w-full bg-green-400 rounded-sm px-2 py-1"
+                : ""
+            }`}
+            href="/blogs/frontend-blogs"
+          >
+            Frontend Blogs
+          </Link>
         </li>
         <li>
-          <Link href="/blogs/backend-blogs">Backend Blogs</Link>
+          <Link
+            className={`w-full ${
+              currentPath === "/blogs/backend-blogs"
+                ? "w-full bg-green-400 rounded-sm px-2 py-1"
+                : ""
+            }`}
+            href="/blogs/backend-blogs"
+          >
+            Backend Blogs
+          </Link>
         </li>
       </ul>
     </div>
