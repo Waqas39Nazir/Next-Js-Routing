a99ac955b52e0eb49f3916a2d62ddc5aeeb5c271
Waqas Nazir <waqasnazir@altaurux.com>
2024-08-11 13:37:51 +0500

    Blogs Page

---
+++ src/app/blogs/[blog-name]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SingleBlog = () => {
+  return (
+    <div>
+      <h1>I am Your Sigle Blog</h1>
+    </div>
+  );
+};
+
+export default SingleBlog;
--- src/components/BlogCard/index.tsx
+++ src/components/BlogCard/index.tsx
@@ -1,4 +1,6 @@
+"use client";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 type Props = {
   title: string;
@@ -6,11 +8,19 @@ type Props = {
 };
 
 const BlogCard = ({ title, description }: Props) => {
+  const router = useRouter();
+
+  const navigationHandler = (title: string) => {
+    router.push(`${title}`);
+  };
   return (
     <div className="flex flex-col h-fit gap-2 rounded-lg text-black bg-green-300 p-3 shadow-md hover:bg-green-500 hover:text-white">
       <h1 className=" text-2xl font-semibold">{title}</h1>
       <p className="text-justify">{description}</p>
-      <button className=" px-3 py-1 rounded bg-yellow-200 hover:bg-yellow-400">
+      <button
+        onClick={() => navigationHandler(title)}
+        className=" px-3 py-1 rounded bg-yellow-200 hover:bg-yellow-400"
+      >
         View
       </button>
     </div>
