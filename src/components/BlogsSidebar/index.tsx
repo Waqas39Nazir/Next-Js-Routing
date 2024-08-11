32838760e884ad8cadf8d41a6e89eea137ac657c
Waqas Nazir <waqasnazir@altaurux.com>
2024-08-11 13:20:43 +0500

    Blog Page Created

--- src/components/BlogsSidebar/index.tsx
+++ src/components/BlogsSidebar/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import Link from "next/link";
 
 const BlogsSidebar = () => {
   return (
     <div className="w-[20%] flex flex-col gap-8 px-10 py-10 bg-black">
       <ul className="flex flex-col gap-5 text-lg font-normal ">
-        <li>Blogs</li>
-        <li>Frontend Blogs</li>
-        <li>Backend Blogs</li>
+        <li>
+          <Link href="/blogs">Blogs</Link>
+        </li>
+        <li>
+          <Link href="/blogs/frontend-blogs">Frontend Blogs</Link>
+        </li>
+        <li>
+          <Link href="/blogs/backend-blogs">Backend Blogs</Link>
+        </li>
       </ul>
     </div>
   );
